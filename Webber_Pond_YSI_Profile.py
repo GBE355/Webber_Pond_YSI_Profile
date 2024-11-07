4497e467611ddd85dfe55ddbbabd10be86b712ad
GBE355 <GBE355@gmail.com>
2024-11-07 16:29:23 -0500

    Update Webber_Pond_YSI_Profile.py

--- Webber_Pond_YSI_Profile.py
+++ Webber_Pond_YSI_Profile.py
@@ -258,4 +258,5 @@ def update_profile_plot(clickData, selected_parameter):
 
 # Run the app
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=True, host="0.0.0.0", port=port)
